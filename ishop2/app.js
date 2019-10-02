054f80160c428b9c844b99940b2ac67a0fe459d4
«233blr» <233blr@gmail.com>
2019-10-02 21:42:42 +0300

    add new file

---
+++ ishop2/app.js
@@ -0,0 +1,26 @@
+﻿var ShopBlock = React.createClass({
+  displayName: "ShopBlock",
+
+  getDefaultProps: function() {
+    return { shopLogo: "Default Name" };
+  },
+
+  render: function() {
+    var itemsCode = [];
+    this.props.catalog.forEach(function(item) {
+      var itemCode = React.DOM.tr(
+        { key: item.id, className: "Item" },
+        React.DOM.td({ className: "ItemTitle" }, item.title),
+        React.DOM.td({ className: "ItemPrice" }, "цена : ", item.price),
+        React.DOM.td({ className: "ItemImage" }, "url : ", item.url),
+        React.DOM.td({ className: "ItemBalance" }, "остаток : ", item.balance)
+      );
+      itemsCode.push(itemCode);
+    });
+    return React.DOM.div(
+      { className: "ShopBlock" },
+      React.DOM.div({ className: "Nomination" }, this.props.shopLogo),
+      React.DOM.tbody({ className: "Items" }, itemsCode)
+    );
+  }
+});
